cdb44109e0897f3b69c06716d4d8fa8b01f78040
Shriyan <shekharshriyan1124@gmail.com>
2024-02-15 13:37:27 +0900

    Changing the 3rd line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -6,7 +6,7 @@ public class mainApp1 {
         int n = 11;
         System.out.println("Welcome to Scientific Calculator");
         System.out.println ("Program Started...");
-        System.out.println ("You can calculate here");
+        System.out.println ("You can calculate numbers");
         System.out.println (b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
